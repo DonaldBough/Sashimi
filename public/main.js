0db82e96e435a82f8eadbe08b598395ac4de17bd
Donald Bough <donaldbough@gmail.com>
2019-02-17 08:09:31 -0500

    Start list logic for ingredient list

--- public/main.js
+++ public/main.js
@@ -1,25 +1,50 @@
 'use strict';
 
+let total = 0;
+
 function addIngredient() {
   let ingredientList = document.getElementById("ingredientList");
+  // call getIngredient correctly so we have id values for list
   let htmlString = getIngredientListElement();
-  let div = document.createElement('div');
+  let listElement = document.createElement('div');
 
-  div.innerHTML = htmlString.trim();
-  ingredientList.append(htmlListItem);
+  listElement.innerHTML = htmlString.trim();
+  ingredientList.append(listElement);
 }
 
-function getIngredientListElement() {
-  return 
+function changeTotal(value, unit) {
+
+}
+
+function getIngredientListElement(id) {
+  let listElementString =  
   `
-    <div id="ingredientListElement" class="input-group mb-3">
-      <input type="text" class="form-control" aria-label="Text input with segmented dropdown button">
-      <select class="custom-select" id="inputGroupSelect01">
-        <option value="g" selected>grams</option>
-        <option value="cup">cups</option>
-        <option value="tsp">tsp</option>
-        <option value="TBSP">TBSP</option>
-      </select>
-    </div>
+    <li>
+      <div id="ingredientListElement" class="input-group mb-3">
+        <input type="text" class="form-control" aria-label="Text input with segmented dropdown button">
+        <select class="custom-select" id="inputGroupSelect01">
+          <option value="g" selected>grams</option>
+          <option value="cup">cups</option>
+          <option value="tsp">tsp</option>
+          <option value="TBSP">TBSP</option>
+        </select>
+        <div class="input-group-append">
+          <button type="button" class="btn addButton">✓</button>
+        </div>
+      </div>
+    </li>
   `;
+  return listElementString;
 }
+
+function listenForAddIngredient() {
+ document.getElementById("ingredientList").addEventListener("click", function(e) {
+    if(e.target && e.target.nodeName == "BUTTON") {
+      console.log("add");
+    }
+  }); 
+}
+
+listenForAddIngredient();
+
+let ingredientListElementArray = [document.getElementById("ingredientListElement0")];
