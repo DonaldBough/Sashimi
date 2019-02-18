c1f140760e5f681eb5a89a2d38c1732e63685510
Donald Bough <donaldbough@gmail.com>
2019-02-17 22:30:32 -0500

    Got everything working

--- public/main.js
+++ public/main.js
@@ -1,10 +1,11 @@
-'use strict';
+'use strict'
 
-let total = 0;
+let gramsInOneCup = 155;
+let tspInCup = 48;
+let tbspInCup = 16;
 
 function addIngredient() {
   let ingredientList = document.getElementById("ingredientList");
-  // call getIngredient correctly so we have id values for list
   let htmlString = getIngredientListElement();
   let listElement = document.createElement('div');
 
@@ -12,39 +13,94 @@ function addIngredient() {
   ingredientList.append(listElement);
 }
 
-function changeTotal(value, unit) {
+function showTotalWeight() {
+  let unitToShowIn = document.getElementById("unitToShowInElement").value;
+  let totalWeight = getTotalWeight(unitToShowIn);
 
+  totalWeight = getConvertedValue(totalWeight, "grams", unitToShowIn);
+  if (totalWeight == 0)
+    document.getElementById("totalWeightElement").value = "Nothing yet..."
+  else
+    document.getElementById("totalWeightElement").value = totalWeight;
 }
 
-function getIngredientListElement(id) {
+function getTotalWeight(unitToShowIn) {
+  let totalWeight =  0;
+  let inputGroups = getInputGroups();
+
+  inputGroups.forEach(function(g) {
+    let weight = getWeightFromGroup(g);
+    let unit = getUnitFromGroup(g);
+
+    totalWeight += getWeightInGrams(weight, unit);
+  });
+  return totalWeight;
+}
+
+function getWeightInGrams(weight, unit) {
+  switch(unit) {
+    case "grams":
+      return weight;
+    case "cups":
+      return weight * gramsInOneCup;
+    case "tsp":
+      return (weight /tspInCup) * gramsInOneCup;
+    case "TBSP":
+      return (weight /tbspInCup) * gramsInOneCup;
+    default:
+      return null;
+  }
+}
+
+function getConvertedValue(weight, unit, unitToShowIn) {
+  switch(unitToShowIn) {
+    case "grams":
+      return weight;
+    case "cups":
+      return weight /gramsInOneCup;
+    case "tsp":
+      return (weight / gramsInOneCup) * tspInCup;
+    case "TBSP":
+      return (weight / gramsInOneCup) * tbspInCup;
+    default:
+      return null;
+  }
+}
+
+function getWeightFromGroup(group) {
+  let weight = group.getElementsByClassName("form-control")[0].value;
+
+  weight = Number(weight);
+  if (isNaN(weight))
+    return 0;
+  return weight;
+}
+
+function getUnitFromGroup(group) {
+  return group.getElementsByClassName("custom-select")[0].value;
+}
+
+function getInputGroups() {
+  let e = document.querySelectorAll(".input-group");
+  let start = 0;
+  let endOfInputForms = e.length -1;
+  return [].slice.call(e, start, endOfInputForms); 
+}
+
+function getIngredientListElement() {
   let listElementString =  
   `
     <li>
       <div id="ingredientListElement" class="input-group mb-3">
         <input type="text" class="form-control" aria-label="Text input with segmented dropdown button">
         <select class="custom-select" id="inputGroupSelect01">
-          <option value="g" selected>grams</option>
-          <option value="cup">cups</option>
+          <option value="grams" selected>grams</option>
+          <option value="cups">cups</option>
           <option value="tsp">tsp</option>
           <option value="TBSP">TBSP</option>
         </select>
-        <div class="input-group-append">
-          <button type="button" class="btn addButton">✓</button>
-        </div>
       </div>
     </li>
   `;
   return listElementString;
 }
-
-function listenForAddIngredient() {
- document.getElementById("ingredientList").addEventListener("click", function(e) {
-    if(e.target && e.target.nodeName == "BUTTON") {
-      console.log("add");
-    }
-  }); 
-}
-
-listenForAddIngredient();
-
-let ingredientListElementArray = [document.getElementById("ingredientListElement0")];
